faa106d81bc24e13eb2b60e9ae3041e984ac3bc9
Arman-Chaudhury-2003 <ac7707@srmist.edu.in>
2025-03-21 14:28:01 +0530

    change from npm to pnpm

--- Backend/index.js
+++ Backend/index.js
@@ -30,4 +30,6 @@ app.use("/api/grievances", grievanceRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
